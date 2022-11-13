51557aed4fd18b76e107e1f5e2a4744a88892f1d
Sarthak Bhatt <sarthakbhatt1407@gmail.comm>
2022-11-13 10:18:21 +0530

    dependencies added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Mail, ShoppingBagRounded } from "@mui/icons-material";
+import AddReactionIcon from "@mui/icons-material/AddReaction";
+import { Badge } from "@mui/material";
 import React from "react";
-
 const App = () => {
-  return <div>App</div>;
+  return <div></div>;
 };
 
 export default App;
